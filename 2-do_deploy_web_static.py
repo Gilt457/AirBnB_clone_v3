37c1db95c4c25f4159994b07adfe7a476548307e
Gilt457 <lepatioborel@gmail.com>
2024-04-26 11:47:56 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Based on 1-pack_web_static.py, this fabric script
+distributes archives to web servers.
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+import os
+from fabric.api import env, put, run
+
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        file_name = os.path.basename(archive_path)
+        base_name = os.path.splitext(file_name)[0]
+        target_path = "/data/web_static/releases/"
+
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(target_path, base_name))
+        run('tar -xzf /tmp/{} -C {}{}/'
+            .format(file_name, target_path, base_name))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(target_path, base_name))
+        run('rm -rf {}{}/web_static'.format(target_path, base_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'
+            .format(target_path, base_name))
         return True
-    except:
+    except Exception:
         return False
