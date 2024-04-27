f1957af94522676729bc06210ee5f860ece28faa
Gilt457 <lepatioborel@gmail.com>
2024-04-26 17:01:25 -0700

    update

--- models/review.py
+++ models/review.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python
-""" Review module """
-from models import storage_t
+#!/usr/bin/python3
+"""Defines the Review class."""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """ Review entity """
-    __tablename__ = 'reviews' if storage_t == 'db' else None
-    if storage_t == 'db':
+    """Class for creating Review objects."""
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'  # Sets the table name for database storage.
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
-        place = relationship("Place", back_populates="reviews")
-        user = relationship("User", back_populates="reviews")
     else:
-        place_id = user_id = text = ""
+        place_id = ""  # Placeholder for place ID when not using a database.
+        user_id = ""  # Placeholder for user ID when not using a database.
+        text = ""  # Placeholder for review text when not using a database.
 
-    def __init__(self, **kwargs):
-        """ Initializes Review instance """
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        """Initializes a new Review instance."""
+        super().__init__(*args, **kwargs)
