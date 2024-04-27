e08b8e55446e270549c16a47fabffb62e8fe662e
Gilt457 <lepatioborel@gmail.com>
2024-04-26 17:34:03 -0700

    update

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-view for Amenity objects that handles all default RestFul API actions
+This script provides an interface for managing Amenity
+objects through standard RESTful API methods.
 """
 from flask import jsonify, abort, request
 from models import storage
@@ -10,26 +11,30 @@ from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
 def handle_amenities():
-    """Retrieves the list of all Amenity objects or
-    create a new Amenity object"""
+    """
+    Fetches a collection of all Amenity entities or
+    establishes a new instance of an Amenity entity.
+    """
     if request.method == 'GET':
         return jsonify([obj.to_dict() for obj in storage.all("Amenity").
-                        values()]), 200
+                       values()]), 200
     if request.method == 'POST':
-            if not request.get_json(silent=True):
-                abort(400, "Not a JSON")
-            if not request.get_json(silent=True).get('name'):
-                abort(400, "Missing name")
-            kwargs = request.get_json(silent=True)
-            new_amenity = Amenity(**kwargs)
-            new_amenity.save()
-            return jsonify(new_amenity.to_dict()), 201
+        if not request.get_json(silent=True):
+            abort(400, "Not a JSON")
+        if not request.get_json(silent=True).get('name'):
+            abort(400, "Missing name")
+        kwargs = request.get_json(silent=True)
+        new_amenity = Amenity(**kwargs)
+        new_amenity.save()
+        return jsonify(new_amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
 def amenity_byid(amenity_id):
-    """Retrieves an Amenity object by id"""
+    """
+    Obtains a specific Amenity entity using its unique identifier.
+    """
     amenity_obj = storage.get("Amenity", amenity_id)
     if amenity_obj:
         if request.method == 'GET':
