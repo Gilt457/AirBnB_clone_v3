f4c3318f95615134878cc726771b15f56547f9a8
Gilt457 <lepatioborel@gmail.com>
2024-04-26 17:14:28 -0700

    update

--- models/state.py
+++ models/state.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
 """This module contains the State class."""
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import models
-import sqlalchemy
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This class is a model representation of a state."""
+    """Class for creating state records."""
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", back_populates="state")
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes a new instance of the State class."""
+        """Constructs a new State instance."""
         super().__init__(*args, **kwargs)
 
     if models.storage_t != "db":
         @property
         def cities(self):
-            """Returns a list of City objects associated with this state."""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            """Returns a collection of City objects associated"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
