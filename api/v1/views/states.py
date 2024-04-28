00aa38d824ef8cf00fe70519d9e9bc54320efaa4
Gilt457 <lepatioborel@gmail.com>
2024-04-28 03:05:37 -0700

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
-state object view for all RestFul API defaults
+view for State objects that handles all default RestFul API actions
 """
 from flask import jsonify, abort, request
 from models import storage
@@ -10,25 +10,25 @@ from models.state import State
 
 @app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def handle_states():
-    """Returns all State objects or creates a new one."""
+    """Retrieves the list of all State objects or create a new State object"""
     if request.method == 'GET':
         return jsonify([obj.to_dict() for obj in storage.all("State").
-                       values()]), 200
+                        values()]), 200
     if request.method == 'POST':
-        if not request.get_json(silent=True):
-            abort(400, "Not a JSON")
-        if not request.get_json(silent=True).get('name'):
-            abort(400, "Missing name")
-        kwargs = request.get_json(silent=True)
-        new_state = State(**kwargs)
-        new_state.save()
-        return jsonify(new_state.to_dict()), 201
+            if not request.get_json(silent=True):
+                abort(400, "Not a JSON")
+            if not request.get_json(silent=True).get('name'):
+                abort(400, "Missing name")
+            kwargs = request.get_json(silent=True)
+            new_state = State(**kwargs)
+            new_state.save()
+            return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
 def state_byid(state_id):
-    """Retrieves, deletes, or updates State objects by id."""
+    """Retrieves a State object by id, delete or update a State object"""
     state_obj = storage.get("State", state_id)
     if state_obj:
         if request.method == 'GET':
@@ -48,4 +48,5 @@ def state_byid(state_id):
                 state_obj.save()
             return jsonify(state_obj.to_dict()), 200
     else:
+        # When the state_id is not linked to any State object
         abort(404)
